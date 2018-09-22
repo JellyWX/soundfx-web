2764904bb36a2c64e9bdd5885a0a304cf20f6136
jude-LafiteIII <judewrs@gmail.com>
2018-09-22 23:36:33 +0100

    basics

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from flask import redirect, render_template, url_for
+from app import app
+
+@app.route('/')
+def index():
+    return redirect(url_for('help'))
+
+@app.route('/help/')
+def help():
+    return render_template('index.html')
---
+++ config.py
@@ -0,0 +1,2 @@
+class Config(object):
+    BASE_URI = './'
