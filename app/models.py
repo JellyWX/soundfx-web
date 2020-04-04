8a4e1225ee02b8228c892a69c8ef366dcd8a17dc
jude-lafitteIII <judewrs@gmail.com>
2020-04-05 00:36:07 +0100

    removed the discord play feature

--- app/models.py
+++ app/models.py
@@ -2,50 +2,51 @@ from app import db
 from sqlalchemy_json import NestedMutableJson
 from sqlalchemy.dialects.mysql import LONGBLOB
 
-
 favorites = db.Table('favorites',
-    db.Column('user_id', db.BigInteger, db.ForeignKey('users.id')),
-    db.Column('sound_id', db.Integer, db.ForeignKey('sounds.id', ondelete='cascade')),
-)
+                     db.Column('user_id', db.BigInteger, db.ForeignKey('users.id')),
+                     db.Column('sound_id', db.Integer, db.ForeignKey('sounds.id', ondelete='cascade')),
+                     )
 
 collections_associated = db.Table('collections_associated',
-    db.Column('collection_id', db.Integer, db.ForeignKey('collections.id', ondelete='cascade')),
-    db.Column('sound_id', db.Integer, db.ForeignKey('sounds.id', ondelete='cascade')),
-)
+                                  db.Column('collection_id', db.Integer,
+                                            db.ForeignKey('collections.id', ondelete='cascade')),
+                                  db.Column('sound_id', db.Integer, db.ForeignKey('sounds.id', ondelete='cascade')),
+                                  )
+
 
 class Server(db.Model):
     __tablename__ = 'servers'
 
-    id = db.Column( db.BigInteger, primary_key=True, autoincrement=False )
-    prefix = db.Column( db.String(5) )
-    roles = db.Column( NestedMutableJson )
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=False)
+    prefix = db.Column(db.String(5))
+    roles = db.Column(NestedMutableJson)
     sounds = db.relationship('Sound', backref='server', lazy='dynamic')
 
 
 class Sound(db.Model):
     __tablename__ = 'sounds'
 
-    id = db.Column( db.Integer, primary_key=True )
-    name = db.Column( db.String(20) )
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
 
-    src = db.Column( LONGBLOB )
-    plays = db.Column( db.Integer )
+    src = db.Column(LONGBLOB)
+    plays = db.Column(db.Integer)
 
-    server_id = db.Column( db.BigInteger, db.ForeignKey('servers.id') )
-    uploader_id = db.Column( db.BigInteger, db.ForeignKey('users.id') )
+    server_id = db.Column(db.BigInteger, db.ForeignKey('servers.id'))
+    uploader_id = db.Column(db.BigInteger, db.ForeignKey('users.id'))
 
-    public = db.Column( db.Boolean, nullable=False, default=True )
+    public = db.Column(db.Boolean, nullable=False, default=True)
 
 
 class Collection(db.Model):
     __tablename__ = 'collections'
 
-    id = db.Column( db.Integer, primary_key=True )
-    name = db.Column( db.String(32) )
-    creator = db.Column( db.String(32) )
-    creation_date = db.Column( db.String(32) )
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+    creator = db.Column(db.String(32))
+    creation_date = db.Column(db.String(32))
 
-    description = db.Column( db.Text )
+    description = db.Column(db.Text)
 
     sounds = db.relationship('Sound', secondary=collections_associated)
 
@@ -53,9 +54,9 @@ class Collection(db.Model):
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column( db.BigInteger, primary_key=True, autoincrement=False )
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=False)
 
-    join_sound_id = db.Column( db.Integer, db.ForeignKey('sounds.id', ondelete='SET NULL'), nullable=True )
+    join_sound_id = db.Column(db.Integer, db.ForeignKey('sounds.id', ondelete='SET NULL'), nullable=True)
     join_sound = db.relationship('Sound', foreign_keys=[join_sound_id])
 
     sounds = db.relationship('Sound', backref='owner', foreign_keys=[Sound.uploader_id])
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,9 @@ from flask import redirect, render_template, request, url_for, session, abort, f
 from sqlalchemy.sql.expression import func
 from app import app, discord, db, limiter
 from app.models import Sound, User, Collection
-import os
-import requests
-import json
 import io
 import subprocess
-import pika
+
 
 def int_or_none(o):
     try:
@@ -21,9 +18,10 @@ def internal_error(error):
     session.clear()
     return "An error has occured! We've made a report, and cleared your cache on this website. If you encounter this error again, please send us a message on Discord!"
 
+
 @app.route('/')
 def index():
-    return redirect( url_for('help') )
+    return redirect(url_for('help'))
 
 
 @app.route('/help/')
@@ -36,25 +34,6 @@ def oauth():
     session.clear()
 
     return redirect(url_for('discord.login'))
-
-
-@app.route('/play/', methods=['POST'])
-@limiter.limit('1 per {} seconds'.format(app.config['PLAY_LIMIT']))
-def play():
-    id = request.args.get('id')
-    user = session.get('user') or discord.get('api/users/@me').json().get('user')
-    s = Sound.query.filter((Sound.public == True) | (Sound.uploader_id == user)).filter(Sound.id == id).first()
-
-    if all(x is not None for x in (s, user, id)):
-        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        channel = connection.channel()
-        channel.queue_declare(queue='soundfx')
-
-        channel.basic_publish(exchange='', routing_key='soundfx', body='{},{}'.format(id, user))
-
-        connection.close()
-
-    return ('OK', 200)
 
 
 @app.route('/fav/', methods=['POST'])
@@ -113,15 +92,20 @@ def dashboard():
         db.session.commit()
 
     if random:
-        s = Sound.query.filter((Sound.public == True) & (Sound.src != None) & (Sound.name.ilike('%{}%'.format(query)))).order_by( func.rand() )
+        s = Sound.query.filter(
+            (Sound.public == True) & (Sound.src != None) & (Sound.name.ilike('%{}%'.format(query)))).order_by(
+            func.rand())
     else:
-        s = Sound.query.filter((Sound.public == True) & (Sound.src != None) & (Sound.name.ilike('%{}%'.format(query)))).order_by( Sound.name )
+        s = Sound.query.filter(
+            (Sound.public == True) & (Sound.src != None) & (Sound.name.ilike('%{}%'.format(query)))).order_by(
+            Sound.name)
 
     max_pages = s.count() // app.config['RESULTS_PER_PAGE']
 
-    s = s.slice(page*app.config['RESULTS_PER_PAGE'], (page+1)*app.config['RESULTS_PER_PAGE'])
+    s = s.slice(page * app.config['RESULTS_PER_PAGE'], (page + 1) * app.config['RESULTS_PER_PAGE'])
 
-    return render_template('dashboard.html', user=u, public=s, q=query, p=page, max_pages=max_pages, title='Dashboard', random=random)
+    return render_template('dashboard.html', user=u, public=s, q=query, p=page, max_pages=max_pages, title='Dashboard',
+                           random=random)
 
 
 @app.route('/collections/')
@@ -138,7 +122,8 @@ def audio():
     s = Sound.query.filter((Sound.public == True) | (Sound.uploader_id == user)).filter(Sound.id == id).first()
 
     if s is not None:
-        sub = subprocess.Popen(('ffmpeg', '-i', '-', '-loglevel', 'error', '-f', 'mp3', 'pipe:1'), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        sub = subprocess.Popen(('ffmpeg', '-i', '-', '-loglevel', 'error', '-f', 'mp3', 'pipe:1'),
+                               stdout=subprocess.PIPE, stdin=subprocess.PIPE)
         stdout = sub.communicate(input=s.src)[0]
 
         return send_file(io.BytesIO(stdout), mimetype='audio/mp3', attachment_filename='{}.mp3'.format(s.name))
